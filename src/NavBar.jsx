516fc02bc2f05ffafc1ee65f46580c634a2770f7
Sergio <alonzos181@gmail.com>
2024-08-03 16:53:49 +0100

    add: deploy

--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -1,9 +1,7 @@
 import React from "react";
 import AppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
-
 import Button from "@mui/material/Button";
-
 import { Box } from "@mui/material";
 import AccountBoxOutlinedIcon from "@mui/icons-material/AccountBoxOutlined";
 import WorkOutlineTwoToneIcon from "@mui/icons-material/WorkOutlineTwoTone";
@@ -14,81 +12,114 @@ import { useTheme } from "@emotion/react";
 import HomeOutlinedIcon from "@mui/icons-material/HomeOutlined";
 import "./styles.css";
 import Container from "@mui/material/Container";
+import { useEffect } from "react";
 
 import { v4 as uuidv4 } from "uuid";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Home from "./Home";
+import Profile from "./Profile";
+import WorkExperience from "./Work Experience";
+import Education from "./Education";
+import Languages from "./languages";
 
 const renderItems = [
-  { title: "Home", text: "Profile", icon: <HomeOutlinedIcon />, id: uuidv4() },
+  {
+    title: "Home",
+    text: "Home",
+    icon: <HomeOutlinedIcon />,
+    id: uuidv4(),
+    path: "profile/",
+  },
   {
     title: "Profile",
     text: "Profile",
     icon: <AccountBoxOutlinedIcon />,
     id: uuidv4(),
+    path: "profile/profile",
   },
   {
     title: "Work Experience",
     text: "WorkExperience",
     icon: <WorkOutlineTwoToneIcon />,
     id: uuidv4(),
+    path: "profile/work_experience",
   },
   {
     title: "Education",
     text: "Education",
     icon: <SchoolOutlinedIcon />,
     id: uuidv4(),
+    path: "profile/education",
   },
   {
     title: "Languages",
     text: "Languages",
     icon: <PublicOutlinedIcon />,
     id: uuidv4(),
+    path: "profile/languages",
   },
 ];
 
 const NavBar = ({ onButtonClick }) => {
   const theme = useTheme();
   const isMobile = useMediaQuery(theme.breakpoints.down("md"));
+
   return (
-    <AppBar position="static" sx={{}}>
-      <Container maxWidth="md">
-        <Toolbar
-          sx={{
-            flexGrow: 1,
-            display: "flex",
-            justifyContent: "space-evenly",
-          }}
-        >
-          {renderItems.map((renderItem) => (
-            <div key={renderItem.id} sx={{}}>
-              <Box sx={{}}>
-                <div>
-                  {!isMobile && (
-                    <Button
-                      sx={{}}
-                      color="inherit"
-                      onClick={() => onButtonClick(renderItem.text)}
-                    >
-                      <Box id="NavButton">
-                        {renderItem.icon}
-                        {renderItem.title}
-                      </Box>
-                    </Button>
-                  )}
-                  {isMobile && (
-                    <Button
-                      color="inherit"
-                      onClick={() => onButtonClick(renderItem.text)}
-                    >
-                      <Box id="NavButton">{renderItem.icon}</Box>
-                    </Button>
-                  )}
-                </div>
-              </Box>
-            </div>
-          ))}
-        </Toolbar>
-      </Container>
-    </AppBar>
+    <Router>
+      <AppBar position="static" sx={{}}>
+        <Container maxWidth="md">
+          <Toolbar
+            sx={{
+              flexGrow: 1,
+              display: "flex",
+              justifyContent: "space-evenly",
+            }}
+          >
+            {renderItems.map((renderItem) => (
+              <Link
+                key={renderItem.id}
+                to={renderItem.path}
+                style={{ color: "inherit", textDecoration: "none" }}
+              >
+                <Box sx={{}}>
+                  <div>
+                    {!isMobile && (
+                      <Button
+                        sx={{}}
+                        color="inherit"
+                        onClick={() => onButtonClick(renderItem.text)}
+                      >
+                        <Box id="NavButton">
+                          {renderItem.icon}
+                          {renderItem.title}
+                        </Box>
+                      </Button>
+                    )}
+                    {isMobile && (
+                      <Button
+                        color="inherit"
+                        onClick={() => onButtonClick(renderItem.text)}
+                      >
+                        <Box id="NavButton">{renderItem.icon}</Box>
+                      </Button>
+                    )}
+                  </div>
+                </Box>
+              </Link>
+            ))}
+          </Toolbar>
+        </Container>
+      </AppBar>
+
+      <Routes>
+        <Route path="profile/" element={<Home />} />
+        <Route path="profile/profile" element={<Profile />} />
+        <Route path="profile/work_experience" element={<WorkExperience />} />
+        <Route path="profile/education" element={<Education />} />
+        <Route path="profile/languages" element={<Languages />} />
+        <Route path="*" element={<Home />} />
+      </Routes>
+    </Router>
   );
 };
 
