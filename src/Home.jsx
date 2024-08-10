ec8ae4ee6ce72ab40c3f34c113578a86be5433c3
Sergio <alonzos181@gmail.com>
2024-08-10 17:44:41 +0100

    adding pictures

--- src/Home.jsx
+++ src/Home.jsx
@@ -5,11 +5,18 @@ import CenteredBox from "./CenteredBox";
 import Divider from "@mui/material/Divider";
 import React, { useState } from "react";
 import { Button, Collapse, Typography } from "@mui/material";
+import "./styles.css";
+
 
 const Home = () => {
   const [open, setOpen] = useState(false);
   const handleClick = () => {
     setOpen(!open);
+  };
+
+  const [open2, setOpen2] = useState(false);
+  const handleClick2 = () => {
+    setOpen2(!open2);
   };
 
   return (
@@ -37,11 +44,11 @@ const Home = () => {
           <Collapse in={open}>
             <p style={{ height: "15px" }}></p>
             <p>
-              I was born and raised in Guatemala in a lovely Catholic family, my
-              dad has always been quite adventurous, he loves to travel to visit
-              the United States all the time and when I was a kid, every time he
-              will leave, I wouldn’t like it, but at the same I would think how
-              would be to get into a plane and go there.
+              I was born and raised in a lovely Catholic family in Guatemala
+              city, my dad has always been quite adventurous, he loves to travel
+              to visit the United States all the time and when I was a kid,
+              every time he will leave, I wouldn’t like it, but at the same I
+              would think how would be to get into a plane and go there.
             </p>
             <p style={{ height: "10px" }}></p>
             <p>
@@ -121,10 +128,56 @@ const Home = () => {
             </p>
             <p style={{ height: "10px" }}></p>
             <p>
-              I am excited about the future, because I know it holds many challenges
-              for me and I have the energy and attitude to take them. If you read
-              this far, uff, let me tell you: Thank you very much!
+              I am excited about the future, because I know it holds many
+              challenges for me and I have the energy and attitude to take them.
+              If you read this far, uff, let me tell you: Thank you very much!
             </p>
+          </Collapse>
+        </CenteredBox>
+      </Box>
+      <Box onClick={handleClick2}>
+        <CenteredBox>
+          <br />
+          {open2 ? (
+            <p>Collapse images</p>
+          ) : (
+            <p>Do you want to see something amazing? click here...</p>
+          )}
+
+          <Collapse in={open2}>
+            <Container maxWidth="lg">
+              <Box sx={{ p: 2, mt: 2 }}>
+                <p className="titlePicture">Antigua Guatemala:</p>
+                <Box sx={{ width: "100%", height: "auto", overflow: "hidden" }}>
+                  <img
+                    src="https://res.cloudinary.com/dvucxxtju/image/upload/v1723306449/20220321_120623_trjlnu.jpg"
+                    alt="Responsive"
+                    style={{ width: "100%", height: "auto", borderRadius: "4px"  }}
+                  />
+                  <img
+                    src="https://res.cloudinary.com/dvucxxtju/image/upload/v1723306451/20220321_153128_sjnata.jpg"
+                    alt="Responsive"
+                    style={{ width: "100%", height: "auto", borderRadius: "4px"  }}
+                  />
+                </Box>
+                <p className="titlePicture">Guatemala City:</p>
+                <Box sx={{ width: "100%", height: "auto", overflow: "hidden" }}>
+                  <img
+                    src="https://res.cloudinary.com/dvucxxtju/image/upload/v1723306450/20221212_104105_2_rmvy6r.jpg"
+                    alt="Responsive"
+                    style={{ width: "100%", height: "auto", borderRadius: "4px"  }}
+                  />
+                  <p className="titlePicture">Ciudad Cayala, Guatemala City:</p>
+                </Box>
+                <Box sx={{ width: "100%", height: "auto", overflow: "hidden"}}>
+                  <img
+                    src="https://res.cloudinary.com/dvucxxtju/image/upload/v1723306449/20220314_124951_kj7hjk.jpg"
+                    alt="Responsive"
+                    style={{ width: "100%", height: "auto", borderRadius: "4px" }}
+                  />
+                </Box>
+              </Box>
+            </Container>
           </Collapse>
         </CenteredBox>
       </Box>
