02b2a027e1e37daf88fe1132a1b5588d8755f6ae
Sergio <alonzos181@gmail.com>
2024-08-11 14:16:32 +0100

    pufff

--- src/main.jsx
+++ src/main.jsx
@@ -12,7 +12,9 @@ const theme = createTheme({
 ReactDOM.createRoot(document.getElementById("root")).render(
   <ThemeProvider theme={theme}>
     <React.StrictMode>
+      <BrowserRouter basename="/profile/">
       <App />
+      </BrowserRouter>
     </React.StrictMode>
   </ThemeProvider>,
   
